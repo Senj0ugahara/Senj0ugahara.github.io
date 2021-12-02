4e5c855e7e36d48ffcbaa35a7ce2be5700f05008
Senj0ugahara <64477219+Senj0ugahara@users.noreply.github.com>
2021-12-02 17:44:16 +0300

    Номера компаний(недоделано)

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@
 		$(this).toggleClass('switch-btn_on');
 	})
 })();
+
+/* numbers__company */
+(function () {
+	$('.numbers-company__form__item__img').on('click', function() {
+		$(this).toggleClass('numbers-company__form__item__img_open');
+    $('.numbers-company__form__box__sub').ed(1).toggleClass('numbers-company__form__box__sub_open') ;
+	})
+})();
